194c34b1c9caf844219974bf6694055af3febc31
Quinn Craddock <qcraddock@technologypartners.net>
2021-06-15 12:31:30 -0500

    Variable name

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -3,16 +3,17 @@ import { useState } from "react"
 
 function Home (){
 
+    // Comment
     let [count, setCount] = useState(0)
 
-    const handleClick = ()=>{
+    const handleClick1 = ()=>{
         setCount(count + 1)
     }
 
     return (
         <>
             <div>Count {count}</div>
-            <button onClick={handleClick} >+ 1</button>
+            <button onClick={handleClick1} >+ 1</button>
         </>
     )
 }
